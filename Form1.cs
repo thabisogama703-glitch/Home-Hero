0b6988d5ac972e41cc762c1adbc924d96805ca0d
thabisogama703-glitch <thabisogama703@gmail.com>
2026-08-14 16:45:51 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Home_Maintenance__Service_Platform
+{
+    public partial class FrmHomeMaintenanceServicePlatform : Form
+    {
+        public FrmHomeMaintenanceServicePlatform()
+        {
+            InitializeComponent();
+        }
+    }
+}
