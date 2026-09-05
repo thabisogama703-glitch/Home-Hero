dde6bf670ae8aa27a729d779f3c87620243198ae
Dusk <shadowsilver308@gmail.com>
2026-09-05 23:34:34 +0200

    Add files via upload

---
+++ Review.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_And_Calculations
+{
+    public class Review
+    {
+        public Guid ReviewId { get; private set; }
+        public int Rating { get; private set; }
+        public string Comment { get; private set; }
+        public DateTime DatePosted { get; private set; }
+        public Job Job { get; private set; }
+        public Customer Customer { get; private set; }
+
+        public Review(Customer customer,Job job,int rating,string comment)
+        {
+         
+                ReviewId = Guid.NewGuid();
+                Customer=customer;
+                Job = job;
+                Rating = rating;
+                Comment = comment;
+                DatePosted = DateTime.Now;
+
+            
+        }
+
+        public bool ReviewValidation(Customer  customer,Job job,int rating , string comment,out Review review,out string error)
+        {
+            if (rating<1 || rating >5)
+            {
+                review = null;
+                error = "Rating must be between 1 and 5";
+                return false;
+            }
+            
+            review = new Review(customer,job,rating,comment);
+            error = null;
+            return true;
+         
+        }
+    }
+}
---
+++ Status.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_And_Calculations
+{
+    
+    
+        public static class JobStatuses
+        {
+         public const string NotStarted = "Not Started";
+         public const string InProgress = "In progress";
+         public const string Completed = "Completed";
+         public const string Paused = "Paused";
+        }
+       
+    
+}
