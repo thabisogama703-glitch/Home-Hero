04b1c6a71f0623e6246a6ad6739ba114c7159657
Alwande Lethuxolo <lethuxolomthembu01@gmail.com>
2026-09-08 01:01:07 +0200

    I managed the requests and admin dashboard

--- Home Hero/AdminDashboard.Designer.cs
+++ Home Hero/AdminDashboard.Designer.cs
@@ -219,7 +219,7 @@
             // 
             button1.Location = new Point(12, 73);
             button1.Name = "button1";
-            button1.Size = new Size(94, 29);
+            button1.Size = new Size(131, 29);
             button1.TabIndex = 8;
             button1.Text = "Overview";
             button1.UseVisualStyleBackColor = true;
